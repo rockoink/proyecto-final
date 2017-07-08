1af563d3de9cf6f705f8c726f313915130657113
Sony <Sony@VAIO>
2017-07-07 21:33:04 -0500

    crear la cuenta de nuevo de mlab.com

---
+++ src/main/java/org/rodrigo/fin/ControladorMensaje.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.rodrigo.fin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Sony
+ */
+@RestController
+@CrossOrigin  //permite que otros servidores accedan a los servicios 
+public class ControladorMensaje {
+    @Autowired RepositorioMensaje repoMensaje;
+    
+    @RequestMapping(value="/guardar-mensaje",method=RequestMethod.GET,headers={"Accept=text/html"})
+
+    public String guardarMensaje(){
+repoMensaje.save(new Mensaje("mi primer registro en Mongo"));
+return "Mensaje guardado";
+}
+    
+    
+}
---
+++ src/main/java/org/rodrigo/fin/Mensaje.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.rodrigo.fin;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ *
+ * @author Sony
+ */
+
+public class Mensaje {
+@Id
+ String id;
+private String titulo;
+
+    public Mensaje(String id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+    }
+
+    public Mensaje(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Mensaje() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    
+}
